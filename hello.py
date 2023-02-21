fce445fa5968545a27f712db6cbf9c163aa5e49b
secmlops <securemlops@gmail.com>
2023-02-21 20:23:19 +0530

    test added

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,5 +7,22 @@ def hello() -> str:
     return "Hello World"
 
 
+@app.route('/nitin')
+def welcome():
+    return 'welcome Nitin, what is your order'
+
+@app.route('/vishnu')
+def greet():
+    return 'Welcome Vishnu ji, what is your order'
+
+@app.route('/hari')
+def namaskar():
+    return 'Welcome Hari ji, what is your order'
+
+@app.route('/<name>')
+def good_morning(name):
+    return 'Welcome ' +  name +', what is your order'
+
+
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="127.0.0.1", port=8080, debug=True)
---
+++ test_20230221.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+myapp = Flask(__name__)
+
+
+
+if __name__ == "__main__":
+    myapp.run()
